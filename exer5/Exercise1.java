bd5542387b6e4fde7d2038d31effe29c4bc2eb22
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-08-08 17:00:36 +0000

    [Add]Exercise1.java

---
+++ exer5/Exercise1.java
@@ -0,0 +1,15 @@
+public class Exercise1 {
+  public static void main(String[] args) {
+    introduceOneself();
+  }
+  public static void introduceOneself() {
+    String name = "wanko"; 
+    int age = 20;
+    double height = 1.5;
+    char zodiac = '牛';
+    System.out.println("私の名前は" + name + "です");
+    System.out.println("年齢は" + age + "歳です");
+    System.out.println("身長は" + height + "mです");
+    System.out.println("干支は" + zodiac + "年です");
+  }
+}
