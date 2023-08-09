6a4ab05c5750aee5358afac3c577d86eddfac3ba
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-08-09 08:45:07 +0000

    [Update]exer5/Exercise2.java

--- exer5/Exercise2.java
+++ exer5/Exercise2.java
@@ -1,10 +1,14 @@
 public class Exercise2 {
   public static void email(String title, String address, String text) {
-    System.out.println(address + "に以下のメールを送信しました");
+    System.out.println(address + "に、以下のメールを送信しました");
     System.out.println("件名：" + title);
     System.out.println("本文：" + text);
   }
   public static void main(String[] args) {
-    email("メール", "a@a", "見えていますか？");
+    //email("メール", "a@a", "見えていますか？");
+    String title = "メール";
+    String address = "a@a";
+    String text = "見えていますか？";
+    email(title, address, text);
   }
 }
