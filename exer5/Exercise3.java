627aae7488a62ecaf1931b78d0958186c0ad04a5
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-08-09 08:47:52 +0000

    [Update]exer5/Exercise3.java

--- exer5/Exercise3.java
+++ exer5/Exercise3.java
@@ -10,7 +10,9 @@ public class Exercise3 {
     System.out.println("本文：" + text);
   }
   public static void main(String[] args) {
-    email("メール", "a@a", "見えていますか？");
-    email("b@b", "オーバーロード");
+    String address = "b@b";
+    String text = "オーバーロード";
+    email(address, text);
+    // email("b@b", "オーバーロード");
   }
 }
