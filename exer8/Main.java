6bd659f7dd4d04bd2d6f37719f2e2c1cfed95f16
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-08-16 02:55:21 +0000

    [Update]exer8/Main.java

--- exer8/Main.java
+++ exer8/Main.java
@@ -1,5 +1,17 @@
 public class Main {
   public static void main(String[] args) {
+    //勇者を生成
+    Hero h = new Hero();
     
+    //フィールドに初期値をセット
+    h.name = "otete";
+    h.hp = 100;
+    System.out.println("勇者" + h.name + "が誕生した！");
+    
+    //勇者の操作
+    h.sit(5);
+    h.slip();
+    h.sit(20);
+    h.run();
   }
 }
