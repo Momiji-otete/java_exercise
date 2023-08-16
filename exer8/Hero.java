b90600c5e0becb8112e8155625f8f33aee847244
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-08-16 02:47:07 +0000

    [Update]exer8/Hero.java slipメソッド追加

--- exer8/Hero.java
+++ exer8/Hero.java
@@ -7,9 +7,15 @@ public class Hero {
     System.out.println(this.name + "は、眠って回復した！");
   }
   
-  pulic void sit(int sec) {
+  public void sit(int sec) {
     this.hp += sec;
     System.out.println(this.name + "は、" + sec "秒座った！");
     System.out.println("HPが" + sec + "ポイント回復した");
+  }
+  
+  public void slip() {
+    this.hp -= 5;
+    System.out.println(this.name + "は、転んだ！");
+    System.out.println("5のダメージ！");
   }
 }
