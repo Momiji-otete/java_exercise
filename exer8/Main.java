c908fa9d71069985ef640d00a118184170a603b9
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-08-16 03:10:11 +0000

    [Update]Main.java

--- exer8/Main.java
+++ exer8/Main.java
@@ -9,9 +9,24 @@ public class Main {
     System.out.println("勇者" + h.name + "が誕生した！");
     
     //勇者の操作
-    h.sit(5);
+    // h.sit(5);
+    // h.slip();
+    // h.sit(20);
+    // h.run();
+    Matango m1 = new Matango();
+    m1.hp = 30;
+    m1.suffix = 'A';
+    System.out.println("お化けキノコ" + m1.suffix + "が現れた！");
+    
+    Matango m2 = new Matango();
+    m2.hp = 30;
+    m2.suffix = 'B';
+    System.out.println("お化けキノコ" + m2.suffix + "が現れた！");
+    
+    //冒険のはじまり
     h.slip();
-    h.sit(20);
+    m1.run();
+    m2.run();
     h.run();
   }
 }
