cf078e5920357065e54908005fe4c45743a33f2a
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-08-18 12:34:09 +0000

    [Add]exer10

---
+++ exer10/Hero.java
@@ -0,0 +1,13 @@
+public class Hero {
+  String name; "otete";
+  int hp = 100;
+
+  public void attack() {
+    System.out.println(this.name + "の攻撃！");
+    System.out.println("5ポイントのダメージを与えた！");
+  }
+
+  public void run() {
+    System.out.println(this.name + "は逃げ出した！");
+  }
+}
