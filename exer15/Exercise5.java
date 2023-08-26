d2b889cccb9be418b818cf927c981d7e1222da24
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-08-26 07:47:37 +0000

    [Add]exer15/Main, Exercise5

---
+++ exer15/Exercise5.java
@@ -0,0 +1,11 @@
+import java.time.*;
+import java.time.format.*;
+
+public class Exercise5 {
+  public static void main(String[] args) {
+    LocalDate now = LocalDate.now();
+    LocalDate future = now.plusDays(100);
+    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("西暦yyyy年MM月dd日");
+    System.out.println(future.format(fmt));
+  }
+}
