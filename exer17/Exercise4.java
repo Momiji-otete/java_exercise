9232d3dcef4b31a1b54255abaae23b9501abf52a
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-08-28 11:18:16 +0000

    [Add]exer17/Exercise4

---
+++ exer17/Exercise4.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Exercise4 {
+  public static void main(String[] args) throws IOException {
+    System.out.println("プログラムが起動しました");
+    throw new IOException();
+  }
+}
