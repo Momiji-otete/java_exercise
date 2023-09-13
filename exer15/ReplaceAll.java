0c634aae806a12ed715aefd6354730f3a6ddebdb
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-09-13 06:51:49 +0000

    [Update]exer15/ReplaceAll.java

--- exer15/ReplaceAll.java
+++ exer15/ReplaceAll.java
@@ -1,9 +1,11 @@
 public class ReplaceAll {
   public static void main(String[] args) {
     String s = "abc,def:ghi";
-    String w = s.replaceAll("[beh]","X");
+    String w = s.replaceAll("[beh]","X"); //正規表現でマッチするものを変更する
     String rf = s.replaceFirst("[beh]","X"); //最初の1つだけを置換する
+    String rp = s.replace("b", "X"); //第一引数が正規表現ではない
     System.out.println(w);
     System.out.println(rf);
+    System.out.println(rp);
   }
 }
